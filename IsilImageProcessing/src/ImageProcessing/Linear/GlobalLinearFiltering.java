bc0e18215020eef4f42cac33979f0e48e68a7464
CofeeTimes <martin.vandenbossche@yahoo.com>
2024-05-04 11:20:50 +0200

    Gnagnagna

--- IsilImageProcessing/src/ImageProcessing/Linear/GlobalLinearFiltering.java
+++ IsilImageProcessing/src/ImageProcessing/Linear/GlobalLinearFiltering.java
@@ -17,17 +17,20 @@ public class GlobalLinearFiltering {
         }
         
         // Fourier Transform
-        MatriceComplexe fourier = Fourier.Fourier2D(imageDouble);
+        MatriceComplexe F = Fourier.Fourier2D(imageDouble);
+        
+        // Cross Cross ... to center
+        MatriceComplexe crossCrossF = Fourier.decroise(F);
 
         // Ideal Low Pass Filter
-        int M = fourier.getLignes();
-        int N = fourier.getColonnes();
+        int M = F.getLignes();
+        int N = F.getColonnes();
         double[][] H = new double[M][N]; // H(u,v)
 
         for (int u = 0; u < M; u++) { 
             for (int v = 0; v < N; v++) {
-                double D = Math.sqrt(u * u + v * v);
-//                double D = Math.sqrt((u - M / 2) * (u - M / 2) + (v - N / 2) * (v - N / 2)); // Dans la littérature ???
+//                double D = Math.sqrt(u * u + v * v); // Fonctionne pas ???
+                double D = Math.sqrt((u - M / 2) * (u - M / 2) + (v - N / 2) * (v - N / 2)); // Dans la littérature ??? Sinon ne fonctionne pas
                 if (D <= cutOffFrequency) { // BW filter
                     H[u][v] = 1;
                 } else {
@@ -36,11 +39,28 @@ public class GlobalLinearFiltering {
             }
         }
         
+        // Apply filter (Multiply and cross cross ...)
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                double realPart = crossCrossF.getPartieReelle()[u][v] * H[u][v];
+                double imaginaryPart = crossCrossF.getPartieImaginaire()[u][v] * H[u][v];
+                crossCrossF.set(u, v, realPart, imaginaryPart);
+            }
+        }
+        MatriceComplexe unCrossCrossF = Fourier.decroise(crossCrossF);
+        
         // Inverse Fourier Transform
-        MatriceComplexe inverseFourier = Fourier.InverseFourier2D(fourier);
+        MatriceComplexe inverseFourier = Fourier.InverseFourier2D(unCrossCrossF);
+        
+        // To double to int (image)
         int[][] imageFiltered = new int[M][N];
-        double[][] realPart = inverseFourier.getPartieReelle();
-        double[][] imaginaryPart = inverseFourier.getPartieImaginaire();
+        double[][] realPart = inverseFourier.getPartieReelle(); // Hope Imaginary part ~= 0
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                imageFiltered[u][v] = (int) Math.round(realPart[u][v]);
+            }
+        }
+   
         return imageFiltered;
     }
 }
